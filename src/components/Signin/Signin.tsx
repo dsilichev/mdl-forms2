4d2b0278b1908ae5b9f1d396ee1c7e98d9cd609f
Dmitrii Ilichev <dsilichev@gmail.com>
2025-09-15 22:51:03 +0300

    signin

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,38 +1,62 @@
-import { useState } from "react";
-import { Input } from "../../components";
+import { useRef, useState } from "react";
+import "./Signin.css";
 
 interface SigninProps {
   onSubmit: (data: { email: string; password: string }) => void;
 }
 
 export const Signin = ({ onSubmit }: SigninProps) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
+  const signinData = useRef({ email: "", password: "" });
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { email, password } = signinData.current;
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit({ email, password });
+    formRef.current?.reset();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    const { name, value } = e.target;
+    signinData.current = { ...signinData.current, [name]: value };
+
+    if (signinData.current.email && signinData.current.password) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+
+  const handleReset = () => {
+    signinData.current = { email: "", password: "" };
+    formRef.current?.reset();
+    setIsDisabled(true);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input
+    <form
+      className="form-container"
+      ref={formRef}
+      onSubmit={handleSubmit}
+      onChange={handleChange}
+      onReset={handleReset}
+    >
+      <input
+        className="input input--default input--radius-sm input--size-md "
         type="email"
-        label="Email"
-        placeholder="your@email.com"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        withAsterisk
+        name="email"
+        placeholder="Your email"
       />
-      <Input
+      <input
+        className="input input--default input--radius-sm input--size-md "
         type="password"
-        label="Password"
+        name="password"
         placeholder="Your password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        withAsterisk
       />
-      <button type="submit">Войти</button>
+      <button type="submit" disabled={isDisabled}>
+        Войти
+      </button>
     </form>
   );
 };
